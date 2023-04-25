c4a4782654a468f9225697ba1af61a6cf37f1ec6
Md_Shafiul_Alam <shafiul.alam@bjitgroup.com>
2023-04-25 12:11:10 +0600

    Initial Commit

---
+++ GeofencingDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeofencingDemo
+//
+//  Created by ShafiulAlam-00058 on 4/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
